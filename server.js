4b1f19c5816b2f33447ebdcc7a6fa34cac8e8e17
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-07-25 16:14:50 -0700

    cowsay is workingish

--- server.js
+++ server.js
@@ -7,25 +7,27 @@ const cowsay = require('cowsay');
 const parseBody = require('./lib/parse-body.js');
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer(function(req, res){
-  req.url = url.parce(req.url);
+const server = http.createServer(function(req, res) {
+  req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
 
-  if(req.method === 'POST') {
-    parseBody(req, function(err){
-      if(err) return console.log(err);
-      console.log('POST request body: req.body');
+  if (req.method === 'POST') {
+    parseBody(req, function(err) {
+      if (err) return console.error(err);
+      console.log('POST request body:', req.body);
     });
   }
 
-  if(req.method === 'GET' && req.url.pathname === '/cowsay'){
+  if (req.method === 'GET' && req.url.pathname === '/cowsay') {
     let params = req.url.query;
     console.log('my querystring paramaters:', params);
-    res.write(cowsay.say({text: params.text}));
+    res.write(cowsay.say({ text: params.text }));
+    res.end();
   }
+
+  res.end();
 });
 
-
-server.listen(PORT, function(){
-  console.log('server', PORT);
+server.listen(PORT, function() {
+  console.log('server up:', PORT);
 });
