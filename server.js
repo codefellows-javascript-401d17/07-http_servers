b9328767f6e8ff17fd64b3663d772437799a306c
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-07-25 15:37:41 -0700

    everything from demo typed in

--- server.js
+++ server.js
@@ -7,7 +7,23 @@ const cowsay = require('cowsay');
 const parseBody = require('./lib/parse-body.js');
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer();
+const server = http.createServer(function(req, res){
+  req.url = url.parce(req.url);
+  req.url.query = querystring.parse(req.url.query);
+
+  if(req.method === 'POST') {
+    parseBody(req, function(err){
+      if(err) return console.log(err);
+      console.log('POST request body: req.body');
+    });
+  }
+
+  if(req.method === 'GET' && req.url.pathname === '/cowsay'){
+    let params = req.url.query;
+    console.log('my querystring paramaters:', params);
+    res.write(cowsay.say({text: params.text}));
+  }
+});
 
 
 server.listen(PORT, function(){
