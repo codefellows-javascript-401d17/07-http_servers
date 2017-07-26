da2130c1dfe52ccbc5124eea7c64f3f54f234083
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-25 21:59:57 -0700

    Added post method

--- server.js
+++ server.js
@@ -8,3 +8,20 @@ const myParse = require('./lib/myParse.js');
 const PORT = process.env.PORT || 3000;
 
 console.log(cowsay.list.toString());
+
+const postMethod = (req, callback) => {
+    parseBody(req, (err) => {
+        if(err) return console.error(err);
+        callback(req)
+    })
+}
+
+const server = http.createServer((req, res) => {
+  req.url = url.parse(req.url);
+    console.log('parsed url: ', req.url);
+    console.log('unparsed query: ', req.url.query)
+    req.url.query = querystring.parse(req.url.query);
+
+     if (req.method === 'POST') postMethod(req.body, 
+      (val) => console.log('POST request body: ', req.body))
+});
