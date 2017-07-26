afbea6c2074399743be400603430f98f63f8e627
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-25 23:16:37 -0700

    Added get method and cowsay path

--- server.js
+++ server.js
@@ -9,32 +9,50 @@ const PORT = process.env.PORT || 3000;
 
 const reqMethods = {};
 
-reqMethods.errorResponse = (req, res) => {
+reqMethods.default = (req, res) => {
   res.writeHead(400, { 'content-type': 'text/plain' });
-  res.write(cowsay.say({text: 'bad request'}));
+  res.write(cowsay.say({text: 'whoops, bad request'}));
+  res.end()
 }
 
-reqMethods.postMethod = (req, res) => {
-  parseBody(req, (err) => {
+reqMethods.POST = (req, res) => {
+  myParse(req, (err) => {
     if (err) console.error(err);
+    if(!req.url.query.text) return reqMethods.default(req, res);
     
-    res.writeHead(400, { 'content-type': 'text/plain' });
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.write(cowsay.say({text: req.url.query.text}));
+    res.end();
+  });    
+}
+
+reqMethods.GET = (req, res) => {
+  myParse(req, (err) => {
+    if (err) console.error(err);
+    if(!req.url.query.text) return reqMethods.default(req, res);
+    
+    res.writeHead(200, { 'content-type': 'text/plain' });
     res.write(cowsay.say({text: req.url.query.text}));
     res.end();
   });    
 }
 
 const server = http.createServer(function(req, res) {
-  req.url = url.parse(requ.url);
-  req.url.query = querystring.parse(req.url.query);
+  req.url = url.parse(req.url);
+  req.url.query = queryString.parse(req.url.query);
 
   if (req.url.pathname === '/') {
     res.writeHead(200, { 'content-type': 'text/plain' });
     res.write('Hello from my server :D')
-    response.end();
+    res.end();
     return;
+  } else if (req.url.pathname === '/cowsay'){
+    console.log(req.method);
+    reqMethods[req.method] ? reqMethods[req.method](req, res):
+    reqMethods.default(req, res);
+  } else {
+    reqMethods.default(req, res);
   }
-
 });
 
 server.listen(PORT, () => {
