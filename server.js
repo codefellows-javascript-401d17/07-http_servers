56504f0a4723e15d87676682c8a5036baface1e2
andyfiveeleven <fouracesguitar@gmail.com>
2017-07-25 22:06:10 -0700

    fixed the post request

--- server.js
+++ server.js
@@ -3,24 +3,54 @@ const url = require('url');
 const querystring = require('querystring');
 const cowsay = require('cowsay');
 const parseBody = require('./lib/parse-body.js');
-const PORT = porcess.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(function(req, res){
   req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
 
-  if (req.method === 'POST'){
-    parseBody(req, function(err){
-      if (err) return console.err(err);
-      console.log('POST request body;', req.body);
+  // if (req.method === 'POST'){
+  //   parseBody(req, function(err){
+  //     if (err) return console.log(err);
+  //   });
+  // }
+
+  if (req.method === 'POST' && req.url.pathname ==='/cowsay') {
+    parseBody(req, function(err) {
+      if (req.body.text) {
+        res.writeHead(200, { 'Content-Type': 'text/plain'});
+        res.write(cowsay.say({ text: req.body.text }));
+        res.end();
+      } else {
+        console.error('error:', err);
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.write(cowsay.say({ text: 'bad POST request'}));
+        res.end();
+      }
     });
   }
 
-  if(req.method === 'get' && req.url.pathname === '/cowsay'){
+  if(req.url.pathname === '/'){
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('hello from my server');
+  }
+
+  if(req.method === 'GET' && req.url.pathname === '/cowsay'){
     let params = req.url.query;
-    console.log('my params', params);
-    res.write(cowsay.say({text: params.text}));
+    if (params.text){
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(cowsay.say({text: params.text, T: 'U', the_cow: 'bong'}));
+    } else {
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.write(cowsay.say({text: 'bad request'}));
+    }
   }
 
   res.end();
+});
+
+server.listen(PORT, function(){
+  console.log('server up on PORT:', PORT);
 });
